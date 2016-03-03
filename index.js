410588684e0b62b0f7fd0fbaa58ed9696d24e410
Ben W <reccanti@gmail.com>
2016-03-03 00:46:59 -0500

    Added React

---
+++ index.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    var Hello = React.createClass({
+        displayName: 'Hello',
+        render: function() {
+            return React.createElement("div", null, "Hello ", this.props.name);
+        }
+    });
+
+    ReactDOM.render(
+        React.createElement(Hello, {name: "World"}),
+        document.getElementById('container')
+    );
+}
