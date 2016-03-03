4a26fe47ee36102fd70f2ac0a8f964fc8df50d67
Ben W <reccanti@gmail.com>
2016-03-03 11:44:26 -0500

    Got Webpack configuration working

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Hello extends React.component {
+class Hello extends React.Component {
     render() {
-        return <h1>Hello</h1>;
+        return (<h1>Hello</h1>);
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-require("webpack");
-
-var config = {
-    context: __dirname,
-    entry: "index.jsx",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    },
-    loaders: [
-        {
-            test: /\.jsx?$/,    
-            loader: 'babel',
-            query: {
-                presets: ["es2015", "react"]
+var path = require('path');
+var webpack = require('webpack');
+ 
+module.exports = {
+  entry: path.resolve(__dirname, "app/index.jsx"),
+  output: { 
+      path: __dirname, 
+      filename: 'dist/bundle.js' 
+  },
+  module: {
+      loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel?cacheDirectory'
             }
-        }
-    ]
-}
+      ]
+  },
+  exclude: path.resolve(__dirname, "node_modules")
+};
